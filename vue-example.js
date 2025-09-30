e0ea9ba21ff40adc540e416e1299b258dd3c7fff
dianannperla <waafirmelyza@hotmail.com>
2025-09-30 15:48:22 +0100

    Update message in Vue data to enhance user experience

--- vue-example.js
+++ vue-example.js
@@ -2,6 +2,6 @@ new Vue({
     el: '#vue-example',
     template: '<h2>This is a Vue.js example!</h2>',
     data: {
-        message: 'Welcome to the Vue.js app!'
+        message: 'Welcome to our awesome Vue.js app!'
     }
 });
