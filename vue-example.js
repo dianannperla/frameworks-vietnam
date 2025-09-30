5b25ccae010f78d95e816379961ca282231f673f
dianannperla <waafirmelyza@hotmail.com>
2025-09-30 15:47:37 +0100

    Updated Vue instance to include data property.

--- vue-example.js
+++ vue-example.js
@@ -1,4 +1,7 @@
 new Vue({
     el: '#vue-example',
-    template: '<h2>This is a Vue.js example!</h2>'
+    template: '<h2>This is a Vue.js example!</h2>',
+    data: {
+        message: 'Welcome to the Vue.js app!'
+    }
 });
