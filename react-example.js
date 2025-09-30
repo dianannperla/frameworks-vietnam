2e12a6bb26329cf978c794a1373be97aa27e1eaf
dianannperla <waafirmelyza@hotmail.com>
2025-09-30 15:44:56 +0100

    feat: initial commit for react-example.js

---
+++ react-example.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+function App() {
+    return <h2>Đây là ví dụ mã React!</h2>;
+}
+
+ReactDOM.render(<App />, document.getElementById('react-example'));
